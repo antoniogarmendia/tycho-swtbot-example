c81ccf4538c6f148031d6894a1a8f77332d3bedf
antoniogarmendia <antonio.agj@gmail.com>
2018-05-16 14:22:11 +0200

    First

---
+++ com.helloworld.tycho/tests/com.helloworld.tycho.rcp.tests/src/com/helloworld/tycho/rcp/tests/ExampleTest.java
@@ -0,0 +1,18 @@
+package com.helloworld.tycho.rcp.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		// just an example
+		/*
+		 * If you want to skip the test execution use mvn clean verify -DskipTests=true
+		 * */
+        assertTrue(true);
+	}
+
+}
