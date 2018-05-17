949a7fab8e6dc4c1dc8165b8f0fe14493e3e9191
antoniogarmendia <antonio.agj@gmail.com>
2018-05-17 13:15:17 +0200

    Update deploy

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+deploy_updates() {
+    # Clone the last two commits of the gh-pages branch
+    rm -rf out || true
+    git clone -b gh-pages --depth 2 --single-branch https://github.com/antoniogarmendia/tycho-swtbot-example.git out
+    cd out
+
+    # Indicate clearly that this commit comes from Travis
+    git config user.name "Travis CI"
+    git config user.email "antonio.agj@gmail.com"
+
+    # If the tip comes from Travis, amend it. Otherwise, add a new commit.
+    rm -rf swtHelloworld-updates
+    cp -r ../com.helloworld.tycho/releng/com.helloworld.tycho.update/target/repository swtHelloworld-updates
+    git add --all .
+    if git log --format=%an HEAD~.. | grep -q "Travis CI"; then
+	      COMMIT_FLAGS="--amend"
+    fi
+    git commit $COMMIT_FLAGS -am "Build update site"
+
+    # Force push to the gh-pages branch
+    git push --force "https://${GH_TOKEN}@${GH_REF}" gh-pages &>/dev/null
+}
+
+# Exit immediately if something goes wrong
+set -o errexit
+
+# Run the regular build
+mvn --quiet clean install
+
+# Only continue deploying to update site for non-PR commits to the master branch
+#if [[ "$TRAVIS_BRANCH" == 'master' && "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+    deploy_updates
+#fi
